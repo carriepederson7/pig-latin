51b610fadc23fc75bdaef2cbf4b987f6e1884163
Student <example@example.com>
2019-08-21 14:19:53 -0700

    Add function to create pig latin word.

--- js/scripts.js
+++ js/scripts.js
@@ -6,25 +6,42 @@ $(document).ready(function() {
     var vowels = ["a","e","i","o","u"]
 
     var phrases = $("#phrase").val();
-    var phraseWords = phrases.split(" ");
-    console.log(phrase);
-    console.log(phraseWords);
-
-    phraseWords.forEach(function(phraseWord) {
-      var wordLetters = phraseWord.split("");
-      for (var i=0; i<wordLetters.length; i++){
-        for (var x=0; i<consonants.length; i++){
-          if (wordLetters[i]===consonants[x]){
-            wordLetters.shift();
 
 
-          }
-        }
-      }
+    var phraseWords = phrases.split("");
+    var firstLetter = phraseWords[0].toUpperCase();
+    phraseWords.shift();
+    var joinedWord = phraseWords.join("");
+    var pigLatinWord = joinedWord.concat(firstLetter + "ay");
+  console.log(pigLatinWord);
 
-console.log(wordLetters);
-    });
 
+
+
+    // console.log(phrases);
+
+
+    // phraseWords.forEach(function(phraseWord) {
+    //   var wordLetters = phraseWord.split("");
+    //   var firstLetter = wordLetters.shift();
+    //   console.log(firstLetter);
+
+      // console.log(wordLetters);
+      //
+      // for (var i=0; i<wordLetters.length; i++){
+      //   for (var x=0; i<consonants.length; i++){
+      //   if (wordLetters[0]===consonants[x]){
+      //
+      //
+      //
+      //     }
+      //   }
+      // }
+      //
+
+    // });
+    //
+    // console.log(wordLetters);
 
 
   });
